9d9fb7db803e0c8c3b6207c5372535464d92d485
Deonis Peretyagin <karudo@gmail.com>
2018-09-05 11:08:27 +0700

    create rpc

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {createRPC} = require('./lib/rpc.js');
 
 const app = express();
 app.set('port', process.env.PORT || 4242);
@@ -8,6 +9,14 @@ app.use(bodyParser.json());
 app.get('/schema', (req, res) => {
   res.json({schema: 'example'});
 });
+
+app.post('/rpc', createRPC({
+  async test (args) {
+    return {
+      withArgs: args
+    }
+  }
+}));
 
 app.listen(app.get('port'), () => {
   console.log('App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'));
